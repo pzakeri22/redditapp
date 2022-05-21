46f41d026872b953c6728f4b7957d6b535cd8ec8
Parisa Z <92223059+pzakeri22@users.noreply.github.com>
2022-05-21 14:31:24 +0100

    Add routing with sample site

--- src/index.js
+++ src/index.js
@@ -4,11 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <BrowserRouter>
+
+    <React.StrictMode> 
+
+      <Routes>
+        <Route path="/" element={<App />} />
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />} />
+          <Route path="*" element={
+              <main style={{ padding: "1rem" }}>
+               <p>There's nothing here! Please go back and try again.</p>
+             </main>
+          }/>
+      </Routes>
+
+    </React.StrictMode>
+
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
